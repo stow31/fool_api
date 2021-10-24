c6c22d88c0d8c31f55323308b44d4e23dadf6318
Sophie Townsend <se.townsend31@gmail.com>
2021-10-24 15:38:35 +1100

    updated scenario db connection

--- model/scenario.js
+++ model/scenario.js
@@ -1,7 +1,22 @@
 const { Pool } = require('pg');
-const db = new Pool({ 
-    database: 'fool_app'
-})
+// const db = new Pool({ 
+//     database: 'fool_app'
+// })
+
+let db;
+if (process.env.NODE_ENV === 'production') {
+  db = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
+} else {
+    db = new Pool({
+    database: 'fool_app',
+    password: 'optional_password'
+  })
+}
 
 const Scenario = {
     findAll: () => {
